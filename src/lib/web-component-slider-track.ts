5aaf825ad4eca577932bfb2f8c896375f252b4c7
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-08-02 21:46:20 -0300

    chore: css config

---
+++ src/lib/web-component-slider-track.ts
@@ -0,0 +1,3 @@
+export class WebComponentSliderTrack extends HTMLElement {
+  connectedCallback(): void {}
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import cssnano from 'cssnano';
+import postcssNested from 'postcss-nested';
+import postcssPixToRem from 'postcss-pxtorem';
 
 export default defineConfig({
   plugins: [
@@ -13,6 +16,18 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'web-component-slider',
       fileName: 'web-component-slider',
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        cssnano,
+        postcssNested,
+        postcssPixToRem({
+          propList: ['*'],
+          rootValue: 16,
+        }),
+      ],
     },
   },
 });
